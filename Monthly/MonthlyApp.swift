3303c1995277d5952ee1933b3353a5a24b61fe54
Peter Hartnett <peter.hartnett@gmail.com>
2022-08-24 10:06:31 -0400

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Peter Hartnett on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
